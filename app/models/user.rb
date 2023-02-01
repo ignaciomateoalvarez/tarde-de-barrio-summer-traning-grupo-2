c5ce6a899b30cdbabdbb8602792f37e274188ecd
Nacho <ignaciomateoalvarez@gmail.com>
2023-02-01 16:27:32 -0300

    Details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-    validates :first_name, :last_name, presence: true
-    validates :first_name, format: { with: %r{\A[a-zA-Z]+\z}  }
-    validates :last_name, format: { with: %r{\A[a-zA-Z]+\z}  }
-    validates :email, presence: true 
-    validates :email, uniqueness: true
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  end
-  
+  validates :first_name, :last_name, presence: true
+  validates :first_name, format: { with: %r{\A[a-zA-Z]+\z}  }
+  validates :last_name, format: { with: %r{\A[a-zA-Z]+\z}  }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'Validation' do 
-    it { is_expected.to validate_presence_of(:first_name) }    
+    it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:email) }   
-     
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
   end
   context 'Factory' do
     it { expect(create(:user)).to be_persisted }
   end
 end
-
-  

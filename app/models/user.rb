39eeb407190280cc1b030c188284f0a4d588c8df
nicoagarcia <nicoagarcia1@gmail.com>
2023-02-02 10:30:15 -0300

    test aplications, validations and css style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
-  validates :first_name, format: { with: %r{\A[a-zA-Z]+\z}  }
-  validates :last_name, format: { with: %r{\A[a-zA-Z]+\z}  }
+  validates :first_name, :last_name, format: { with: %r{\A[a-zA-Z]+\z} }
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }

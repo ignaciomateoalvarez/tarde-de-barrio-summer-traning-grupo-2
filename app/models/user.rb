ce04a981119edc4a9f808ecaec3b8a8808b88de7
nicoagarcia <nicoagarcia1@gmail.com>
2023-02-09 10:37:37 -0300

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,3 +9,4 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i }
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
 end
+ 

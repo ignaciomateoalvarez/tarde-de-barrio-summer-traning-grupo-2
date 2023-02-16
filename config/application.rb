cce45d9e828f6d290d7098766b4309827c1902c8
carlaibelc <carlaibelc@gmail.com>
2023-02-16 13:57:12 -0300

    - Traducciones

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module StGrupo2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = "es"
+    config.i18n.default_locale = "en"
   end
 end

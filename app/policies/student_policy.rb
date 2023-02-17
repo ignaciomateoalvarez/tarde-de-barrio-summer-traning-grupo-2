0ce02f3c167c30950af6bca750e114441dfb266a
Gianlucas <gianlucasgalati11@gmail.com>
2023-02-17 12:33:13 -0300

    Policy Student

---
+++ app/policies/student_policy.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class StudentPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    user.administrador? || user.colaborador?
+  end
+
+  def show?
+    user.administrador? || user.colaborador?
+  end
+
+  def create?
+    user.administrador? || user.colaborador?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.administrador? || (user.colaborador? && record.user_id == user.id)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+end

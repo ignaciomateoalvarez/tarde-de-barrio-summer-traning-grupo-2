de675de22b48a68b39059c6616cd5501541d9a7d
Gianlucas <gianlucasgalati11@gmail.com>
2023-02-22 11:38:23 -0300

    Modal Delete

---
+++ app/javascript/controllers/turbo_delete_modal_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="turbo-modal"
+export default class extends Controller {
+	static targets = ["delete-modal"];
+	connect() {
+		document.getElementById("modal-background").style.pointerEvents = "none";
+	}
+
+	closeModal() {
+		this.element.parentElement.removeAttribute("src");
+		this.element.remove();
+	}
+
+	submitEnd(e) {
+		if (e.detail.success) {
+			this.closeModal();
+		}
+	}
+}
